ebedb25fa9ad8d81310e5017edbeeca78367ede5
shabs76 <shabsmwanga76@gmail.com>
2023-12-27 19:08:24 +0300

    adding get driver details

--- userApp/code/auth/processes/getInfoProcess.js
+++ userApp/code/auth/processes/getInfoProcess.js
@@ -54,6 +54,98 @@ class getInfoProcessClass extends mainActsClass{
         }
         return sc;
     }
+
+    getAllDriverInformation = async (driver_id) => {
+        const driverInf = await authDbObj.selectDriverDetails([driver_id, 'active'], " `driver_id` = ? AND `status` = ?");
+        if (!_.isArray(driverInf)) {
+            this.Mlogger.error(driverInf);
+            const er = {
+                state: 'error',
+                data: 'An error occurred while fetching driver details',
+                adv: 'logout'
+            }
+            return er;
+        } else if (_.isArray(driverInf) && _.isEmpty(driverInf)) {
+            const er = {
+                state: 'error',
+                data: 'Taarifa za akaunti yako zinakosekana. Tafadhali jaribu tena baadae',
+                adv: 'logout'
+            }
+            return er;
+        }
+        
+        const driverOb = driverInf[0];
+        // delete password data 
+        if (typeof (driverOb.password) === 'string') {
+            delete driverOb.password;
+        }
+        // get park information park_area
+        const parkInf = await authDbObj.selectParkAreasDetails([driverOb.park_area]);
+        if (!_.isArray(parkInf)) {
+            this.Mlogger.error(parkInf);
+            const er = {
+                state: 'error',
+                data: 'An error has occurred while fetching parking information'
+            }
+            return er;
+        } else if (_.isArray(parkInf) && _.isEmpty(parkInf)) {
+            const er = {
+                state: 'error',
+                data: 'Taarifa juu ya kituo chako hazipatikani kwasasa. Tafadhali rudia tena'
+            }
+            return er;
+        }
+
+        // now get leadership information if exits
+        const ledInf = await authDbObj.selectParkLeaders([driver_id, 'active'], " `driver_id` = ? AND `status` = ? ");
+        if (!_.isArray(ledInf)) {
+            this.Mlogger.error(ledInf);
+            const er = {
+                state: 'error',
+                data: 'An error has occurred while fetching leadership information'
+            }
+            return er;
+        } else if(_.isArray(ledInf) && _.isEmpty(ledInf)) {
+            driverOb.parkName = parkInf[0].park_name;
+            driverOb.leadership = 'Mwanachama'
+            const sc = {
+                state: 'success',
+                data:  driverOb
+            }
+            return sc;
+        }
+
+        // now fetch leadership type
+        const letyNames = await authDbObj.selectLeaderTypes([ledInf[0].leader_type]);
+        if (_.isArray(letyNames)) {
+            this.Mlogger.error(letyNames);
+            driverOb.parkName = parkInf[0].park_name;
+            driverOb.leadership = 'Kiongozi'
+            const sc = {
+                state: 'success',
+                data:  driverOb
+            }
+            return sc;
+        } else if (_.isArray(letyNames) && _.isEmpty(letyNames)) {
+            this.Mlogger.error(`Driver of id ${driver_id} has unknown leadership type`);
+            driverOb.parkName = parkInf[0].park_name;
+            driverOb.leadership = 'Kiongozi'
+            const sc = {
+                state: 'success',
+                data:  driverOb
+            }
+            return sc;
+        }
+
+        driverOb.parkName = parkInf[0].park_name;
+        driverOb.leadership = letyNames[0].type_name;
+        const sc = {
+            state: 'success',
+            data:  driverOb
+        }
+        return sc;
+        
+    }
 }
 
 const GettorObj = new getInfoProcessClass();
--- userApp/routers/getters.js
+++ userApp/routers/getters.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import GettorObj from '../code/auth/processes/getInfoProcess.js';
-
+import authProcessObj from '../code/auth/processes/authProcess.js';
 
 const router = express.Router();
 
@@ -26,7 +26,62 @@ router.get('/reqistration/info', async (req, res) => {
         }
     }
     res.json(com);
-})
+});
 
+
+
+
+router.get('/driver/details', async (req, res) => {
+    const hedz = req.headers;
+    if (typeof (hedz.drlogkey) !== 'string' || typeof (hedz.drlogsess) !== 'string') {
+        const er = {
+            state: 'error',
+            data: 'Missing login details information',
+            adv: 'logout'
+        }
+        return res.json(er);
+    }
+    // check if the login info are valid still (info.logKey) !== 'string' || typeof (info.logSess)
+    const info = {
+        logKey: hedz.drlogkey,
+        logSess: hedz.drlogsess
+    };
+   const ansLog = await authProcessObj.checkDriversLoginStatus(info);
+   if (ansLog.state !== 'success') {
+     ansLog.adv = 'logout';
+     return res.json(ansLog);
+   }
+
+   const ansInf = await GettorObj.getAllDriverInformation(ansLog.driver_id);
+   res.json(ansInf);
+
+});
+
+
+// from here information requires login
+
+router.use(async (req, res, next) => {
+    // check for headers
+    const hedz = req.headers;
+    if (typeof (hedz.drlogkey) !== 'string' || typeof (hedz.drlogsess) !== 'string') {
+        const er = {
+            state: 'error',
+            data: 'Missing login details information',
+            adv: 'logout'
+        }
+        return res.json(er);
+    }
+    // check if the login info are valid still (info.logKey) !== 'string' || typeof (info.logSess)
+    const info = {
+        logKey: hedz.drlogkey,
+        logSess: hedz.drlogsess
+    };
+   const ansLog = await authProcessObj.checkDriversLoginStatus(info);
+   if (ansLog.state !== 'success') {
+     ansLog.adv = 'logout';
+     return res.json(ansLog);
+   }
+   next();
+});
 const geterRouter = router;
 export default geterRouter;
