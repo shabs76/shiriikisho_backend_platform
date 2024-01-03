19c12699eb011bc324d628ec1a311126fe3ad116
shabs76 <shabsmwanga76@gmail.com>
2024-01-03 10:08:06 +0300

    adding back text opts

--- userApp/code/auth/processes/authProcess.js
+++ userApp/code/auth/processes/authProcess.js
@@ -205,23 +205,23 @@ class authProcess extends mainActsClass {
         }
 
         // send otp to user 
-        // const textInfo = {
-        //     phone,
-        //     sms: otp+' Itakuwa namba ya udhibitisho kwenye mfumo wa shirikisho.'
-        // }
-        // const sendTextAns = await this.sendNormalTexts(textInfo);
-        // if (sendTextAns.state !== 'success') {
-        //     const er = {
-        //         state: 'error',
-        //         data: 'Mfumo umeshindwa kutuma ujumbe uwa udhibitisho. Tafadhali jaribu tena'
-        //     }
-        //     return er;
-        // }
+        const textInfo = {
+            phone,
+            sms: otp+' Itakuwa namba ya udhibitisho kwenye mfumo wa shirikisho.'
+        }
+        const sendTextAns = await this.sendNormalTexts(textInfo);
+        if (sendTextAns.state !== 'success') {
+            const er = {
+                state: 'error',
+                data: 'Mfumo umeshindwa kutuma ujumbe uwa udhibitisho. Tafadhali jaribu tena'
+            }
+            return er;
+        }
 
         // return success with code to track verification
         const sc = {
             state: 'success',
-            data: 'Namba ya uthibitisho imetumwa kwenye number ya simu +'+phone+'-'+otp,
+            data: 'Namba ya uthibitisho imetumwa kwenye number ya simu +'+phone,
             otp_id: savOtp.data.otp_id
         }
         return sc;
@@ -268,22 +268,22 @@ class authProcess extends mainActsClass {
         }
 
         // send new otp
-        // const textInfo = {
-        //     phone: otAns[0].phone,
-        //     sms: infCo.otp+' Itakuwa namba ya uthibitisho kwenye mfumo wa shirikisho.'
-        // }
-        // const sendTextAns = await this.sendNormalTexts(textInfo);
-        // if (sendTextAns.state !== 'success') {
-        //     const er = {
-        //         state: 'error',
-        //         data: 'Mfumo umeshindwa kutuma ujumbe uwa uthibitisho. Tafadhali jaribu tena'
-        //     }
-        //     return er;
-        // }
+        const textInfo = {
+            phone: otAns[0].phone,
+            sms: infCo.otp+' Itakuwa namba ya uthibitisho kwenye mfumo wa shirikisho.'
+        }
+        const sendTextAns = await this.sendNormalTexts(textInfo);
+        if (sendTextAns.state !== 'success') {
+            const er = {
+                state: 'error',
+                data: 'Mfumo umeshindwa kutuma ujumbe uwa uthibitisho. Tafadhali jaribu tena'
+            }
+            return er;
+        }
 
         const sc = {
             state: 'success',
-            data: 'Ujumbe wa number ya umetumwa kwenye simu +'+otAns[0].phone+'-'+infCo.otp
+            data: 'Ujumbe wa number ya umetumwa kwenye simu +'+otAns[0].phone
         }
 
         return sc;
